f7a0e22d228b746fa4f7f4afe851f7b1f98d9f46
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-12-25 18:13:39 +0530

    rightsidebar done
    clerk/themes installed

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -22,6 +23,7 @@ export const TopBar = () => {
             </SignedIn>
           </div>
           <OrganizationSwitcher appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: 
               "py-2 px-4"
